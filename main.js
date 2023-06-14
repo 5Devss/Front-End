f7711cbc0b95857269856e37eb09e4c1c91054e1
Guilherme Sella <guilhermesella1974@gmail.com>
2023-06-13 22:20:20 -0300

    fix hamburguer

--- main.js
+++ main.js
@@ -5,11 +5,20 @@ window.addEventListener("scroll", () => {
 });
 
 const menuHambuguer = document.querySelector("#menu-hamburguer")
+const linkMobile = document.querySelectorAll("#linkM")
 menuHambuguer.addEventListener("click", () => {
     let nav = document.querySelector(".navMobile")
    
     nav.classList.toggle('active')
 })
+
+function fecharNav(){
+    let nav = document.querySelector(".navMobile")
+    let input = document.querySelector("#menu-hamburguer")
+    input.checked = false;
+    nav.classList.toggle('active')
+}
+
 const fecharMobile = ()=>{
     let nav = document.querySelector(".navMobile")
     nav.classList.remove('active')
