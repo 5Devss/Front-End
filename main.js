06513c71c6bb2505c36a936356e6ea5d966abc1f
Guilherme Sella <guilhermesella1974@gmail.com>
2023-07-06 14:27:34 -0300

    first commit

--- main.js
+++ main.js
@@ -27,3 +27,37 @@ const fecharMobile = ()=>{
     removerFundo()
 }
 
+//FUNÇÃO SUBMIT
+
+
+function submitForm(event) {
+    event.preventDefault();
+    
+    var form = document.getElementById("contactForm");
+    var formData = new FormData(form);
+    
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://formsubmit.co/magolokovirus@gmail.com", true);
+    
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                showConfirmation();
+            } else {
+                alert("Ocorreu um erro ao enviar o formulário. Por favor, tente novamente mais tarde.");
+            }
+        }
+    };
+    
+    xhr.send(formData);
+}
+
+function showConfirmation() {
+    alert("Ótimo, recebemos seu E-mail, Entraremos em contato");
+}
+function scrollToContact() {
+    var contatoSection = document.getElementById("contato");
+    var offset = 175; // Ajuste este valor para definir a quantidade de deslocamento para cima
+    var topPos = contatoSection.offsetTop - offset;
+    window.scrollTo({ top: topPos, behavior: "smooth" });
+}
